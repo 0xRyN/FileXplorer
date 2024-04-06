8595469dd0f8642e9edb241d5d521e75a621764c
Rayan <ryanalaz11@gmail.com>
2024-04-06 15:08:41 +0200

    🚀 First Commit!

---
+++ src/main/java/org/xplorer/Main.java
@@ -0,0 +1,12 @@
+package org.xplorer;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Xplorer");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+    }
+}
